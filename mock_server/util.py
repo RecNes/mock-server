a33ed0e0edb1e57adbfedcc94e19ef497819a886
LaZyQ <zyqhi2010@gmail.com>
2016-10-09 21:07:20 +0800

    fix TypeError: must be unicode, not str

--- mock_server/util.py
+++ mock_server/util.py
@@ -44,7 +44,7 @@ class ExtendedJSONEncoder(json.JSONEncoder):
 
 
 def slugify(value, delimiter="-"):
-    slug = unicodedata.normalize("NFKD", value).encode("ascii", "ignore")
+    slug = unicodedata.normalize("NFKD", unicode(value)).encode("ascii", "ignore")
     slug = re.sub(r"[^\w]+", " ", slug)
     return delimiter.join(slug.lower().strip().split())
 
